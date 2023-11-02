454159f0461c4d629365c3deea3d1cf4c92cec5b
MartinMandzak <mandzak.martin3@gmail.com>
2023-11-02 13:48:00 +0100

    kekw

--- bublacek.py
+++ bublacek.py
@@ -2,6 +2,6 @@
 
 
 
-data=[x for x in list(map(int,input('nums: ').split(' ')))];print(f"{[data[idx] for idx in range(len(data)-1) if [(lambda x,y: x<y)(data[i],data[idx+1]) for i in range(len(data)-1)][idx]]}")
+data=[x for x in list(map(int,input('nums: ').split(' ')))];print(f"{[data[idx] if [(lambda x,y: x<y)(data[i],data[idx+1]) for i in range(len(data))][idx] else 'kokot' for idx in range(len(data)-1)]}")
 
 
