5aa04ed710520f2566fa5de53411b61be9f0d051
MartinMandzak <mandzak.martin3@gmail.com>
2023-11-02 14:58:37 +0100

    Throwing in the towel for now

--- bublacek.py
+++ bublacek.py
@@ -1,7 +1,9 @@
 
+"""
+Trying to do bubblesort like this but it's hard and a elcture is starting
+"""
 
 
-
-data=[x for x in list(map(int,input('nums: ').split(' ')))];print(f"{[data[idx] if [(lambda x,y: x<y)(data[i],data[idx+1]) for i in range(len(data))][idx] else 'kokot' for idx in range(len(data)-1)]}")
+data=[x for x in list(map(int,input('nums: ').split(' ')))];print(f"{[x for x in [(lambda: data.insert(0, data.pop(idx))) if [(lambda x,y: x<y)(data[i],data[idx+1]) for i in range(len(data)-1)][idx] else 'kokot' for idx in range(len(data)-1)]]}")
 
 
