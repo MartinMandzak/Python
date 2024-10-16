ff17429caa3030fd9603b5e6a49907024b99ff2c
Martin Mandzák <mandzak.martin3@gmail.com>
2024-10-16 09:22:36 +0200

    retardometer

--- retard.py
+++ retard.py
@@ -1,4 +1,6 @@
-import matplotlib as mpl
+import matplotlib.pyplot as mpl
+import pandas as pd
+import csv
 
 keys = ['bed', 'lamp', 'spasm', 'wardrobe', 'backpack', 'left', 'belt']
 
@@ -9,14 +11,36 @@ def new():
             if keys.index(key) != len(keys)-1: file.write(',')
 
 def question():
-    pass
-
-def append(answers): # array
-    pass
+    question_bank = ['stood up and sat down on his bed ','turned his lamp on and off ', 'had a spasm on his bed ', 'opened his wardrobe ', 'fucked with his backpack ', 'leave the room ', 'fucked with his belt ']
+    with open("./retard.csv", "a") as file:
+        print("How many times did he: ")
+        for question in question_bank:
+            answer = str(input(question))
+            file.write(answer)
+            if question_bank.index(question) != len(question_bank)-1: file.write(',')
 
 def load():
-    pass
+    with open("./retard.csv", newline='') as file:
+        read_data = []
+        retard_watcher = csv.reader(file, delimiter = ',')
+
+        for row in retard_watcher:
+            read_data.append(row)
+            print(row)
+
+        return np.array(read_data)
 
 def chart():
-    pass
+    data = pd.read_csv('./retard.csv')
+    
+    for column in data.columns:
+        mpl.plot(data.index+1, data[column], marker='o', linestyle='-', label = column)
+    mpl.xlabel("Days")
+    mpl.ylabel("Amt. of times")
+    mpl.title("Retard-o-meter")
 
+    mpl.legend()
+    mpl.grid(True)
+    mpl.show()
+
+chart()
